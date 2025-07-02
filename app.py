e8fa595bd0b19561d98f64c4f3ee893007906c72
KJ <74859235+giorno420@users.noreply.github.com>
2025-07-02 13:36:56 +0100

    added graphs

--- app.py
+++ app.py
@@ -24,35 +24,35 @@ if st.session_state.page == 0:
         #General Questions
         st.header("Company 🏢")
 
-        name = st.text_input("Name of the organisation:")
-        Registration_options = st.selectbox("where was the organisation registered?",["Choose a country","United Kingdom", "USA", "France", "Germany", "India", "Australia", "Japan"])
-        Date_of_Foundation = st.date_input("Select the date the company was created:",value=date.today())
-        Location_of_HQ = st.selectbox("Where is the location of the organisation's headquarters?",["Choose a country","United Kingdom", "USA", "France", "Germany", "India", "Australia", "Japan"])
+        st.session_state.name = st.text_input("Name of the organisation:")
+        st.session_state.registration_location = st.selectbox("where was the organisation registered?",["Choose a country","United Kingdom", "USA", "France", "Germany", "India", "Australia", "Japan"])
+        st.session_state.foundation_date = st.date_input("Select the date the company was created:",value=date.today())
+        st.session_state.location = st.selectbox("Where is the location of the organisation's headquarters?",["Choose a country","United Kingdom", "USA", "France", "Germany", "India", "Australia", "Japan"])
 
         #Environmental Questions
         st.header("Environment 🌲")
 
-        st.session_state.fossil_fuels = st.radio("Does the organisation have any involvement with fossil fuels?", ["Yes","No"],)
-        st.session_state.energy_consumption = st.number_input("What is the company's annual energy consumption(kWh)?")
-        st.session_state.waste = st.number_input("What is the company's annual waste generation(kg)?")
-        st.session_state.recycle = st.number_input("How much is recycled per year(kg)?")
-        st.session_state.Carbon_Emissions = st.number_input("What is the total annual carbon emissions for this company(tonnes)?")
+        st.session_state.fossil_fuels = st.radio("Does the organisation have any involvement with fossil fuels?", ["Yes","No"], index=None)
+        st.session_state.energy_consumption = st.number_input("What is the company's annual energy consumption(kWh)?", value=None)
+        st.session_state.waste = st.number_input("What is the company's annual waste generation(kg)?", value=None)
+        st.session_state.recycle = st.number_input("How much is recycled per year(kg)?", value=None)
+        st.session_state.carbon_emissions = st.number_input("What is the total annual carbon emissions for this company(tonnes)?", value=None)
 
         #Social Questions
         st.header("Social 🤝")
 
-        st.session_state.Employees = st.number_input("Total number of employees?")
-        st.session_state.F_Employees = st.number_input("Total number of female and non-binary employees?")
-        st.session_state.Training_Hours = st.number_input("Total individual employee training hours per year?")
-        st.session_state.Outreach = st.radio("Does the organisation have any outreach programs?", ["Yes","No"],)
-        st.session_state.Volunteer_Hours = st.number_input("Total individual employee volunteer hours per year?")
+        st.session_state.employees = st.number_input("Total number of employees?", value=None)
+        st.session_state.female_employees = st.number_input("Total number of female and non-binary employees?", value=None)
+        st.session_state.training_hours = st.number_input("Total individual employee training hours per year?", value=None)
+        st.session_state.outreach = st.radio("Does the organisation have any outreach programs?", ["Yes","No"], index=None)
+        st.session_state.volunteer_hours = st.number_input("Total individual employee volunteer hours per year?", value=None)
 
         #Governance Questions
         st.header("Governance ⚖️")
 
-        st.session_state.Risk_management = st.radio("Is there a risk management process in place?", ["Yes","No"],)
-        st.session_state.Cybersecurity = st.radio("Is there a cybersecurity policy?", ["Yes","No"],)
-        st.session_state.Whistleblower = st.radio("Is there a whistle-blower policy?", ["Yes","No"],)
+        st.session_state.risk_management = st.radio("Is there a risk management process in place?", ["Yes","No"], index=None)
+        st.session_state.cybersecurity = st.radio("Is there a cybersecurity policy?", ["Yes","No"], index=None)
+        st.session_state.whistleblower = st.radio("Is there a whistle-blower policy?", ["Yes","No"], index=None)
 
         st.button("Submit", on_click=nextpage, disabled=(st.session_state.page > 3))
 
@@ -65,28 +65,34 @@ elif st.session_state.page == 1:
         st.header("Environment 🌲")
 
         energy_consumption = st.session_state.get("energy_consumption", None)
+        done = True
+        for val in st.session_state.values():
+            if val is None:
+                done = False
+                break
+        # if not done:
+        #     st.warning("Some fields are not filled in yet.")
+            
+        # else:
+        if True:
 
-        if energy_consumption is None:
-            st.warning("Please fill out the 'Questions' page first.")
-        else:
             labels = ["Energy Consumption (This Company)", "General Energy Consumption"]
             values = [energy_consumption, 500000]
 
-            fig, ax = plt.subplots()
-            ax.bar(labels, values, color=["red", "yellow"])
-            ax.set_ylabel("Energy consumption (kWh)")
-            ax.set_title("Consumption")
+            consumptionFig, consumptionAx = plt.subplots()
+            consumptionAx.bar(labels, values, color=["green" if energy_consumption < 500000 else "red", "green"])
+            consumptionAx.set_ylabel("Energy consumption (kWh)")
+            consumptionAx.set_title("Consumption")
 
-            st.pyplot(fig)
+            st.pyplot(consumptionFig)
 
-        waste = st.session_state.get("waste", None)
-        recycle = st.session_state.get("recycle", None)
+            waste = st.session_state.get("waste", None)
+            recycle = st.session_state.get("recycle", None)
 
-        if waste is None or recycle is None :
-            st.warning("Please fill out the 'Questions' page first.")
-        else:
-            df = px.data.tips()
-            fig2 = px.pie(df, values='tip', names='day')
-            st.plotly_chart(fig2)
-        
-        st.button("Restart", on_click=restart, disabled=(st.session_state.page > 3))
+            
+            wasteFig, wasteAx = plt.subplots()
+            wasteAx.pie([recycle, waste - recycle], labels=["Recycled", "Waste"], autopct='%1.1f%%', colors=["green", "red"])
+            wasteAx.set_title("Waste Management")
+            st.pyplot(wasteFig)
+            
+            st.button("Restart", on_click=restart, disabled=(st.session_state.page > 3))
