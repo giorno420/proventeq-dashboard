ad7300b925dcf4c2287604b5a0b27c9e169e6e4d
JiyaR847 <jiyarohit08@gmail.com>
2025-07-03 11:45:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -42,27 +42,28 @@ if st.session_state.page == 0:
         #General Questions
         st.header("Company 🏢")
 
-        st.session_state.name = st.text_input("Name of the organisation:")
+        st.session_state.name = st.text_input("Name of the organisation:",value = st.session_state.get("name",None))
         st.session_state.registration_location = st.selectbox("where was the organisation registered?",["Choose a country","United Kingdom", "USA", "France", "Germany", "India", "Australia", "Japan"])
         st.session_state.foundation_date = st.date_input("Select the date the company was created:",value=date.today())
         st.session_state.location = st.selectbox("Where is the location of the organisation's headquarters?",["Choose a country","United Kingdom", "USA", "France", "Germany", "India", "Australia", "Japan"])
+
 
         #Environmental Questions
         st.header("Environment 🌲")
 
         st.session_state.fossil_fuels = st.radio("Does the organisation have any involvement with fossil fuels?", ["Yes","No"], index=None)
-        st.session_state.energy_consumption = st.number_input("What is the company's annual energy consumption(kWh)?", value=100000)
-        st.session_state.waste = st.number_input("What is the company's annual waste generation(kg)?", value=2)
-        st.session_state.recycle = st.number_input("How much is recycled per year(kg)?", value=1)
-        st.session_state.carbon_emissions = st.number_input("What is the total annual carbon emissions for this company(tonnes)?", value=1000)
+        st.session_state.energy_consumption = st.number_input("What is the company's annual energy consumption(kWh)?",value = st.session_state.get("energy_consumption",None))
+        st.session_state.waste = st.number_input("What is the company's annual waste generation(kg)?", value = st.session_state.get("waste",None))
+        st.session_state.recycle = st.number_input("How much is recycled per year(kg)?", value = st.session_state.get("recycle",None))
+        st.session_state.carbon_emissions = st.number_input("What is the total annual carbon emissions for this company(tonnes)?",value = st.session_state.get("carbon_emissions",None))
 
         #Social Questions
         st.header("Social 🤝")
 
-        st.session_state.employees = st.number_input("Total number of employees?", value=5)
-        st.session_state.female_employees = st.number_input("Total number of female and non-binary employees?", value=1)
+        st.session_state.employees = st.number_input("Total number of employees?",value = st.session_state.get("employees",None))
+        st.session_state.female_employees = st.number_input("Total number of female and non-binary employees?",value = st.session_state.get("female_employees",None))
         st.session_state.outreach = st.radio("Does the organisation have any outreach programs?", ["Yes","No"], index=None)
-        st.session_state.volunteer_hours = st.number_input("Total volunteer/outreach hours per year?", value=200)
+        st.session_state.volunteer_hours = st.number_input("Total volunteer/outreach hours per year?",value = st.session_state.get("volunteer_hours",None))
 
         #Governance Questions
         st.header("Governance ⚖️")
